f1b23ef049c57dbd2abc7710add0470500592a7b
고재현 <121279571+gojaeng@users.noreply.github.com>
2023-07-04 17:24:03 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 128 ms, Memory: 9652 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const input = require("fs").readFileSync("/dev/stdin").toString();
+
+let answer = "";
+let blank = "";
+
+for (i = 1; i <= input; i++) {
+    answer += "*";
+    for (let j = 0; j < input - i; j++) {
+        blank += " ";
+    }
+    console.log(blank + answer);
+    blank = "";
+}
