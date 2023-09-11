d23116f95fd1c78d1fb8c78ff3f747bf2f1b94f2
고재현 <121279571+gojaeng@users.noreply.github.com>
2023-09-11 12:20:01 +0900

    [Silver V] Title: 단어 정렬, Time: 260 ms, Memory: 19944 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+input.shift();
+arr = [...new Set(input)];
+
+arr = arr.sort((a, b) => a.length - b.length || a.localeCompare(b)).join('\n');
+console.log(arr);
