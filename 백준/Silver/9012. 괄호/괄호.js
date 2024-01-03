7464ee8cf236bed3bad4fe51b06cfab003caa747
고재현 <121279571+gojaeng@users.noreply.github.com>
2024-01-03 23:48:23 +0900

    [Silver IV] Title: 괄호, Time: 136 ms, Memory: 9524 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const Num = Number(input[0]);
+
+for (let i = 1; i <= Num; i++) {
+    const cases = input[i];
+    const stack = [];
+    let result = 'YES';
+
+    for (let j = 0; j < cases.length; j++) {
+        if (cases[j] === '(') {
+            stack.push(1);
+        } else {
+            if (!stack.pop()) {
+                result = 'NO';
+                break;
+            }
+        }
+    }
+    if (stack.length !== 0) {
+        result = 'NO';
+    }
+
+    console.log(result);
+}
